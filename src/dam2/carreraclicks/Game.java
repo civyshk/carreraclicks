097d6b9f89b6ac108d0c3f5dee3f9f5848514c3b
Carlos Aguirre Vozmediano <38355125+CodigoBase2018@users.noreply.github.com>
2018-09-19 22:36:30 +0200

    Primeros pasos con GAME
    Necesito la GUI WindowName para obtener el nombre y crear un objeto Player.

--- src/dam2/carreraclicks/Game.java
+++ src/dam2/carreraclicks/Game.java
@@ -2,46 +2,47 @@ package dam2.carreraclicks;
 
 public class Game {
 
-    private Gui gui;
     private Player localPlayer;
-    private NetManager netManager;
+//    private Gui gui;
+//    private NetManager netManager;
 
-    /* TODO Crear jugador, crear gui, crear netManager, mostrar primera ventana */
-
+    /* TODO Crear juga(dor, crear gui, crear netManager, mostrar primera ventana */
+    
+    public Game(){
+        // NECESITO LA GUI PRINCIPAL WindowName para poder continuar.
+        // 1 - GUI WindowName - Obtener nombre del jugador y asignarselo, esto ultimo se hace al crear el objeto Player mediante el constructor.
+        // 2 - GUI WindowMainMenu - Obtener la IP del jugador y asignarsela al jugador, usando los metodos setIP(cadena).
+        // 3 - A partir de este punto el jugador ya deberia tener tanto IP como nombre.
+    }
+    
     // Desde WindowName
-
     /**
      * Establece el nombre del jugador local
      * @param name
      */
-    protected void setPlayerName(String name){}
+    protected void setPlayerName(String name){
+    }
 
     // Desde WindowMainMenu
-
     /**
      * Indica que el usuario desea conectarse a partidas existentes
      */
-    protected void initClientMode(){}
+    protected void initClientMode() {
+    }
 
     /**
      * Indica que el usuario desea crear una nueva partida
      */
-    protected void initServerMode(){}
+    protected void initServerMode() {
+    }
 
-
-
-
-
-
-
-
-    /** Si el jugador cancela unirse a una partida o crearla */
-    protected void regretMode(){}
-
-    protected void setGameName(String gameName){}
-
-    protected void startGame(){}
-
-    protected void joinGame(String ip){}
-
+    
+    
+    
+    /**
+     * Metodo principal Crea el juego
+     */
+    public static void main(String[] args) {
+        new Game();
+    }
 }
